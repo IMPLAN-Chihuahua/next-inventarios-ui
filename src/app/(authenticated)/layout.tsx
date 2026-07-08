3857295648605e63d4da5e0cc7647e59ce9753c2
Paloma <90.paloma@gmail.com>
2026-07-08 13:05:49 -0600

    Agregar color de fondo a todas las paginas excepto login

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 import AuthProvider from "./AuthProvider";
 import NavBar from '@/src/components/general/functional/NavBar';
 import ThemeRegistry from '@/src/components/ThemeRegistry';
@@ -10,18 +11,39 @@ export const metadata = {
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
+      <body style={{ margin: 0 }}>
         <ThemeRegistry>
+          <CssBaseline /> 
           <AuthProvider>
-            <NavBar/> 
             
-            {/* contenedor paginas */}
-            <div>
-              {children}
-            </div>
+            <Box
+              sx={{
+                bgcolor: '#f5f5f7',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <NavBar />
+
+              <Box
+                component="main"
+                sx={{
+                  flexGrow: 1,
+                  pt: { xs: '90px', md: '130px' },
+                  px: { xs: 2, md: 4, lg: 6 },
+                  maxWidth: 1440,
+                  mx: 'auto',
+                  width: '100%',
+                }}
+              >
+                {children}
+              </Box>
+            </Box>
+
           </AuthProvider>
         </ThemeRegistry>
       </body>
     </html>
-  );
+  ); 
 }
